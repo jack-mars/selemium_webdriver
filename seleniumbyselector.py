36328894e454cbef6c38256f94861ec7653aea47
jack-mars <43054536+jack-mars@users.noreply.github.com>
2018-10-16 10:07:15 -0700

    Update seleniumbyselector.py

--- seleniumbyselector.py
+++ seleniumbyselector.py
@@ -2,5 +2,4 @@ from selenium import webdriver
 import time
 driver = webdriver.Chrome('C:/Users/jackh/Downloads/chromedriver_win32/chromedriver')
 driver.get('https://www.baidu.com/')
-driver.find_element_by_css_selector("#kw").send_keys("inty youtube\n")
-#return
+driver.find_element_by_css_selector("#kw").send_keys("inty youtube\n"
